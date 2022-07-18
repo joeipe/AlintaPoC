c4e2e112380a01ee9d4a6500ad53aa292f132fc4
Joe Ipe <Joe.Ipe@alintaenergy.com.au>
2022-07-18 11:36:49 +1000

    ManagementHelper

---
+++ src/AlintaPoC.Messages/AlintaPoC.Messages.MessagingBus/ManagementHelper.cs
@@ -0,0 +1,110 @@
+﻿using Azure;
+using Azure.Messaging.ServiceBus.Administration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlintaPoC.Messages.MessagingBus
+{
+    public class ManagementHelper
+    {
+        private readonly ServiceBusAdministrationClient _administrationClient;
+
+        public ManagementHelper(string connectionString)
+        {
+            _administrationClient = new ServiceBusAdministrationClient(connectionString);
+        }
+
+        public async Task CreateQueueAsync(string queueName)
+        {
+            Console.Write("Creating queue {0}...", queueName);
+            var description = GetQueueDescription(queueName);
+            var createdDescription = await _administrationClient.CreateQueueAsync(description);
+            Console.WriteLine("Done!");
+        }
+
+        public async Task DeleteQueueAsync(string queueName)
+        {
+            Console.Write("Deleting queue {0}...", queueName);
+            await _administrationClient.DeleteQueueAsync(queueName);
+            Console.WriteLine("Done!");
+        }
+
+        public async Task ListQueuesAsync()
+        {
+            AsyncPageable<QueueProperties> allQueueProperties = _administrationClient.GetQueuesAsync();
+            List<QueueProperties> queuePropertiesList = await allQueueProperties.ToListAsync();
+            Console.WriteLine("Listing queues...");
+            foreach (QueueProperties queueProperties in queuePropertiesList)
+            {
+                Console.WriteLine("\t{0}", queueProperties.Name);
+            }
+            Console.WriteLine("Done!");
+        }
+
+        public async Task GetQueueAsync(string queuePath)
+        {
+            QueueProperties queueProperties = await _administrationClient.GetQueueAsync(queuePath);
+            Console.WriteLine($"Queue description for {queuePath}");
+            Console.WriteLine($"    Name:                                   {queueProperties.Name}");
+            Console.WriteLine($"    MaxSizeInMegabytes:                     {queueProperties.MaxSizeInMegabytes}");
+            Console.WriteLine($"    RequiresSession:                        {queueProperties.RequiresSession}");
+            Console.WriteLine($"    RequiresDuplicateDetection:             {queueProperties.RequiresDuplicateDetection}");
+            Console.WriteLine($"    DuplicateDetectionHistoryTimeWindow:    {queueProperties.DuplicateDetectionHistoryTimeWindow}");
+            Console.WriteLine($"    LockDuration:                           {queueProperties.LockDuration}");
+            Console.WriteLine($"    DefaultMessageTimeToLive:               {queueProperties.DefaultMessageTimeToLive}");
+            Console.WriteLine($"    DeadLetteringOnMessageExpiration:       {queueProperties.DeadLetteringOnMessageExpiration}");
+            Console.WriteLine($"    EnableBatchedOperations:                {queueProperties.EnableBatchedOperations}");
+            Console.WriteLine($"    MaxSizeInMegabytes:                     {queueProperties.MaxSizeInMegabytes}");
+            Console.WriteLine($"    MaxDeliveryCount:                       {queueProperties.MaxDeliveryCount}");
+            Console.WriteLine($"    Status:                                 {queueProperties.Status}");
+        }
+
+        public async Task CreateTopicAsync(string topicName)
+        {
+            Console.Write("Creating topic {0}...", topicName);
+            var description = await _administrationClient.CreateTopicAsync(topicName);
+            Console.WriteLine("Done!");
+        }
+
+        public async Task CreateSubscriptionAsync(string topicName, string subscriptionName)
+        {
+            Console.Write("Creating subscription {0}/subscriptions/{1}...", topicName, subscriptionName);
+            var description = await _administrationClient.CreateSubscriptionAsync(topicName, subscriptionName);
+            Console.WriteLine("Done!");
+        }
+
+        public async Task ListTopicsAndSubscriptionsAsync()
+        {
+            AsyncPageable<TopicProperties> allTopicProperties = _administrationClient.GetTopicsAsync();
+            List<TopicProperties> topicPropertiesList = await allTopicProperties.ToListAsync();
+            Console.WriteLine("Listing topics and subscriptions...");
+            foreach (TopicProperties topicProperties in topicPropertiesList)
+            {
+                Console.WriteLine("\t{0}", topicProperties.Name);
+                AsyncPageable<SubscriptionProperties> allSubscriptionProperties = _administrationClient.GetSubscriptionsAsync(topicProperties.Name);
+                List<SubscriptionProperties> subscriptionPropertiesList = await allSubscriptionProperties.ToListAsync();
+                foreach (SubscriptionProperties subscriptionProperties in subscriptionPropertiesList)
+                {
+                    Console.WriteLine("\t\t{0}", subscriptionProperties.SubscriptionName);
+                }
+            }
+            Console.WriteLine("Done!");
+        }
+
+        public CreateQueueOptions GetQueueDescription(string name)
+        {
+            return new CreateQueueOptions(name)
+            {
+                //RequiresDuplicateDetection = true,
+                //DuplicateDetectionHistoryTimeWindow = TimeSpan.FromMinutes(10),
+                //RequiresSession = true,
+                //MaxDeliveryCount = 20,
+                //DefaultMessageTimeToLive = TimeSpan.FromHours(1),
+                //EnableDeadLetteringOnMessageExpiration = true
+            };
+        }
+    }
+}
--- src/AlintaPoC.Messages/AlintaPoC.Messages.Sender/Program.cs
+++ src/AlintaPoC.Messages/AlintaPoC.Messages.Sender/Program.cs
@@ -14,20 +14,116 @@ namespace AlintaPoC.Messages.Sender
             var serviceBusConnectionString = "Endpoint=sb://alintapocmessaging.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=UY6efbRVDvNzW1BL5a4soZB6qT1iZX/nI/Qgq6DGu98=";
             var topicName = "persontopic";
 
-            /*
-            // Create an administraion client to manage artifacts
-            var serviceBusAdministrationClient = new ServiceBusAdministrationClient(serviceBusConnectionString);
-
-            //Create a topic if it doesnot exist
-            if (!await serviceBusAdministrationClient.TopicExistsAsync(topicName))
-            {
-                await serviceBusAdministrationClient.CreateTopicAsync(topicName);
-            }
-            */
+            Manage(serviceBusConnectionString);
 
             MessageBus messageBus = new MessageBus(new AzServiceBus(serviceBusConnectionString));
             var person = new { id = 0, firstName = "J", lastName = "Bond", doB = "26/04/1981" };
             messageBus.SendMessage(person, topicName);
+        }
+
+        private static void Manage(string serviceBusConnectionString)
+        {
+            var helper = new ManagementHelper(serviceBusConnectionString);
+
+            bool done = false;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write(">");
+                string commandLine = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                string[] commands = commandLine.Split(' ');
+
+                try
+                {
+                    if (commands.Length > 0)
+                    {
+                        switch (commands[0])
+                        {
+                            case "createqueue":
+                            case "cq":
+                                if (commands.Length > 1)
+                                {
+                                    helper.CreateQueueAsync(commands[1]).Wait();
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine("Queue path not specified.");
+                                }
+                                break;
+                            case "listqueues":
+                            case "lq":
+                                helper.ListQueuesAsync().Wait();
+                                break;
+                            case "getqueue":
+                            case "gq":
+                                if (commands.Length > 1)
+                                {
+                                    helper.GetQueueAsync(commands[1]).Wait();
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine("Queue path not specified.");
+                                }
+                                break;
+                            case "deletequeue":
+                            case "dq":
+                                if (commands.Length > 1)
+                                {
+                                    helper.DeleteQueueAsync(commands[1]).Wait();
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine("Queue path not specified.");
+                                }
+                                break;
+                            case "createtopic":
+                            case "ct":
+                                if (commands.Length > 1)
+                                {
+                                    helper.CreateTopicAsync(commands[1]).Wait();
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine("Topic path not specified.");
+                                }
+                                break;
+                            case "createsubscription":
+                            case "cs":
+                                if (commands.Length > 2)
+                                {
+                                    helper.CreateSubscriptionAsync(commands[1], commands[2]).Wait();
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine("Topic path not specified.");
+                                }
+                                break;
+                            case "listtopics":
+                            case "lt":
+                                helper.ListTopicsAndSubscriptionsAsync().Wait();
+                                break;
+                            case "exit":
+                                done = true;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                }
+
+            } while (!done);
         }
     }
 }
